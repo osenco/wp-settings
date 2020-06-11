72f2d9010d9c34672a9beb1093e4d4aa4ea4d846
Mauko Maunde <maukoese@gmail.com>
2020-06-11 07:48:41 +0300

    Update Example.php

--- Example.php
+++ Example.php
@@ -1,29 +1,46 @@
 <?php
 
-class MySettings extends Base
+/**
+ * @package WooCommerce Notifications via Africa's Talking
+ * @link https://osen.co.ke
+ * @version 0.20.60
+ * @since 0.20.40
+ * @author Osen Concepts < hi@osen.co.ke >
+ */
+
+namespace My\Namespace;
+
+use Osen\Wp\Settings\Base;
+
+class MySettings
 {
+
+    private $settings;
+
     private $statuses = [
-        'created', 'pending', 'failed', 'on-hold',  'processing', 'completed', 'refunded', 'cancelled'
+        'created', 'pending', 'failed', 'on-hold', 'processing', 'completed', 'refunded', 'cancelled',
     ];
 
-    function __construct()
+    public function __construct()
     {
+        $this->settings = new Base;
+
         add_action('admin_init', array($this, 'admin_init'));
         add_action('admin_menu', array($this, 'admin_menu'), 99);
     }
 
-    function admin_init()
+    public function admin_init()
     {
 
         //set the settings
-        $this->set_sections($this->get_settings_sections());
-        $this->set_fields($this->get_settings_fields());
+        $this->settings->set_sections($this->get_settings_sections());
+        $this->settings->set_fields($this->get_settings_fields());
 
         //initialize settings
-        $this->admin_init();
+        $this->settings->admin_init();
     }
 
-    function admin_menu()
+    public function admin_menu()
     {
         add_submenu_page(
             'woocommerce',
@@ -31,22 +48,26 @@ class MySettings extends Base
             'SMS Notifications',
             'manage_options',
             'at_notify',
-            array($this, 'plugin_page')
+            array($this, 'settings_page')
         );
     }
 
-    function get_settings_sections()
+    public function get_settings_sections()
     {
         $sections = array(
             array(
-                'id'    => 'gateway',
-                'title' => __('Admin Options', 'woocommerce')
-            )
+                'id'      => 'gateway',
+                'title'   => __('Admin Options', 'woocommerce'),
+                'heading' => __('Admin Options', 'woocommerce'),
+                'desc'    => 'You can use placeholders such as <code>{first_name}</code>, <code>{last_name}</code>, <code>{order}</code>, <code>{site}</code>, <code>{phone}</code> to show customer names, order number, website name and customer phone respectively.'
+            ),
         );
         foreach ($this->statuses as $status) {
             $sections[] = array(
-                'id'    => $status,
-                'title' => ucwords($status)
+                'id'      => $status,
+                'title'   => ucwords($status),
+                'heading' => 'On ' . ucwords($status) . ' Status',
+                'desc'    => 'You can use placeholders such as <code>{first_name}</code>, <code>{last_name}</code>, <code>{order}</code>, <code>{site}</code>, <code>{phone}</code> to show customer names, order number, website name and customer phone respectively.'
             );
         }
 
@@ -58,39 +79,39 @@ class MySettings extends Base
      *
      * @return array settings fields
      */
-    function get_settings_fields()
+    public function get_settings_fields()
     {
         $settings_fields = array(
             'gateway' => array(
                 array(
-                    'name'              => 'key',
-                    'label'             => __('API Key', 'woocommerce'),
-                    'type'              => 'text',
-                    'placeholder'           => 'Your API Key',
-                    'sanitize_callback' => 'sanitize_text_field'
-                ),
-                array(
                     'name'              => 'username',
-                    'label'             => __('API Username', 'woocommerce'),
+                    'label'             => __('AT Username', 'woocommerce'),
                     'type'              => 'text',
-                    'placeholder'           => 'Your API Username',
-                    'sanitize_callback' => 'sanitize_text_field'
+                    'placeholder'       => 'Your Africa\'s Talking API Username',
+                    'sanitize_callback' => 'sanitize_text_field',
                 ),
                 array(
                     'name'              => 'shortcode',
-                    'label'             => __('Sender ID', 'woocommerce'),
+                    'label'             => __('AT Sender ID', 'woocommerce'),
                     'type'              => 'text',
-                    'placeholder'           => 'Your Sender ID',
-                    'sanitize_callback' => 'sanitize_text_field'
+                    'placeholder'       => 'Your Africa\'s Talking Sender ID',
+                    'sanitize_callback' => 'sanitize_text_field',
+                ),
+                array(
+                    'name'              => 'key',
+                    'label'             => __('AT API Key', 'woocommerce'),
+                    'type'              => 'text',
+                    'placeholder'       => 'Your Africa\'s Talking API Key',
+                    'sanitize_callback' => 'sanitize_text_field',
                 ),
                 array(
                     'name'        => 'phones',
                     'label'       => __('Admin Numbers', 'woocommerce'),
                     'desc'        => __('Comma-separated list of numbers to notify on status change', 'woocommerce'),
                     'type'        => 'textarea',
-                    'placeholder' => 'E.g 254...,255...,256..'
-                )
-            )
+                    'placeholder' => 'E.g 254...,255...,256..',
+                ),
+            ),
         );
 
         foreach ($this->statuses as $status) {
@@ -99,70 +120,50 @@ class MySettings extends Base
                     'name'  => 'customer_enable',
                     'label' => __('Customer Enable', 'woocommerce'),
                     'desc'  => __('Notify customer on ' . $status . ' status', 'woocommerce'),
-                    'type'  => 'checkbox'
+                    'type'  => 'checkbox',
                 ),
                 array(
-                    'name'        => 'customer_msg',
-                    'label'       => __('Customer Message', 'woocommerce'),
-                    'desc'        => __('Message to send to customer for ' . $status . ' status', 'woocommerce'),
-                    'type'        => 'textarea',
-                    'default'     => 'Hello {customer}, your order on {site} is ' . $status . '.'
+                    'name'    => 'customer_msg',
+                    'label'   => __('Customer Message', 'woocommerce'),
+                    'desc'    => __('Message to send to customer for ' . $status . ' status', 'woocommerce'),
+                    'type'    => 'textarea',
+                    'default' => 'Hello {first_name}, your order on {site} is ' . $status . '.',
                 ),
                 array(
                     'name'  => 'admin_enable',
                     'label' => __('Admin Enable', 'woocommerce'),
                     'desc'  => __('Notify admin(s) on ' . $status . ' status', 'woocommerce'),
-                    'type'  => 'checkbox'
+                    'type'  => 'checkbox',
                 ),
                 array(
-                    'name'        => 'admin_msg',
-                    'label'       => __('Admin Message', 'woocommerce'),
-                    'desc'        => __('Message to send to admin(s) for ' . $status . ' status', 'woocommerce'),
-                    'type'        => 'textarea',
-                    'rows'        => 2,
-                    'default'     => 'An order is ' . $status . ' on {site}.'
-                )
+                    'name'    => 'admin_msg',
+                    'label'   => __('Admin Message', 'woocommerce'),
+                    'desc'    => __('Message to send to admin(s) for ' . $status . ' status', 'woocommerce'),
+                    'type'    => 'textarea',
+                    'rows'    => 2,
+                    'default' => 'An order is ' . $status . ' on {site}.',
+                ),
             );
         }
 
         return $settings_fields;
     }
 
-    function get_option($option, $section = 'gateway', $default = '')
+    public function get_option($option, $section = 'gateway', $default = '')
     {
         $options = get_option($section);
         return $options[$option] ?? $default;
     }
 
-    function plugin_page()
+    public function settings_page()
     {
         echo '<div class="wrap">';
 
         echo '<h1>' . esc_html(get_admin_page_title()) . '</h1>';
 
-        $this->show_navigation();
-        echo '<p>You can use placeholders such as <code>{customer}</code>, <code>{order}</code>, <code>{site}</code>, <code>{phone}</code> to show customer name, order number, website name and customer phone respectively.</p>';
-
-        $this->show_forms();
+        $this->settings->show_navigation();
+        $this->settings->show_forms();
 
         echo '</div>';
-    }
-
-    /**
-     * Get all the pages
-     *
-     * @return array page names with key value pairs
-     */
-    function get_pages()
-    {
-        $pages = get_pages();
-        $pages_options = array();
-        if ($pages) {
-            foreach ($pages as $page) {
-                $pages_options[$page->ID] = $page->post_title;
-            }
-        }
-
-        return $pages_options;
     }
 }
